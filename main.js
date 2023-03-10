48d32b6313986a60eac44af8cf75df08ecee503f
Warhair <tegzesmarton@gmail.com>
2023-03-10 11:01:30 +0100

    galeriamegvan

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
-window.addEventListener("load",init)
-function init(){
-    const KEPEK = ["59.jpg", "images.jif", "images2.jif","img3.png","img4.png","img6.png","img7.png","img8.png"];
-    const ARTICEELEM = document.querySelectorAll("article")
-    for (let index = 0; index < KEPEK.length; index++) {
-        ARTICEELEM[index].innerHTML +=`<div><img src= ${KEPEK[index]} alt='kep'></div>`;
-        
-    }
-    
-    
+let KEPEK = [
+  "59.jpg",
+  "images.jfif",
+  "images2.jfif",
+  "img3.jpg",
+  "img4.jpg",
+  "img6.jfif",
+  "img7.jpg",
+  "img8.jfif"
+];
+window.addEventListener("load", init);
+function init() {
+  let szoveg = "";
+  const ARTICEELEM = document.querySelectorAll("article");
+  const ButtonELEM = document.querySelectorAll("section button");
+  ButtonELEM[0].addEventListener("click", csere);
+  for (let i = 0; i < KEPEK.length; i++) {
+    szoveg += `<div><img src='${KEPEK[
+      i
+    ]} 'alt='kep' onclick= 'changeImg(${i})'></div>`;
+    const DIVELEM = document.querySelectorAll("article div");
+    ARTICEELEM[0].innerHTML = szoveg;
+  }
+}
+
+function changeImg(i) {
+  const image = document.querySelectorAll("section div img");
+  image[0].src = KEPEK[i];
+}
+
+function csere() {
+  i++;
+  document.querySelectorAll("article div img").src = KEPEK[i];
+  if (i == KEPEK.length - 1) {
+    i = -1;
+  }
 }
